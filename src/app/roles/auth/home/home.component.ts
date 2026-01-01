3cf0fd1639794adb33886a347c5479d25e7cba90
Umesh <2463711@cognizant.com>
2026-01-01 18:45:37 +0530

    created footer

--- src/app/roles/auth/home/home.component.ts
+++ src/app/roles/auth/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { FeaturesComponent } from './features/features.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent,HeroComponent,FeaturesComponent],
+  imports: [NavbarComponent,HeroComponent,FeaturesComponent,FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
